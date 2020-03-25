2ff47e258f668ee2565d07407d730cb8291f84f7
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-03-25 17:05:55 +0000

    sets up several album and artist instances

--- db/console.rb
+++ db/console.rb
@@ -1,3 +1,54 @@
 require("pry")
 require_relative("../models/album")
 require_relative("../models/artist")
+
+
+artist1 = Artist.new({
+  'name' => 'David Bowie'
+  })
+
+artist1.save
+
+artist2 = Artist.new({
+  'name' => 'John Maus'
+  })
+
+artist2.save
+
+artist3 = Artist.new({
+  'name' => 'Kino'
+  })
+
+artist3.save
+
+album1 = Album.new({
+  'title' => 'Low',
+  'genre' => 'Rock/Electronic',
+  'artist_id' => artist1.id
+  })
+
+album1.save
+
+album2 = Album.new({
+  'title' => 'Heroes',
+  'genre' => 'Rock/Electronic',
+  'artist_id' => artist1.id
+  })
+
+  album2.save
+
+  album3 = Album.new({
+    'title' => 'We Must Become Pitiless Censors of Ourselves',
+    'genre' => 'Electronic',
+    'artist_id' => artist2.id
+    })
+
+  album3.save
+
+  album4 = Album.new({
+    'title' => 'Gruppa Krovi',
+    'genre' => 'Rock',
+    'artist_id' => artist3.id
+    })
+
+  album4.save
--- models/artist.rb
+++ models/artist.rb
@@ -10,5 +10,13 @@ class Artist
     @id = options['id'].to_i if options['id']
   end
 
+  def save
+    sql = "INSERT INTO artists (name)
+           VALUES ($1)
+           RETURNING id"
+    values = [@name]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
 
 end
